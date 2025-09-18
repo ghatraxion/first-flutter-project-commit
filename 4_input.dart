27d73925f52529ff64b37940439392db76f65072
ghatraxion <agh.arrafif@gmail.com>
2025-09-18 13:16:32 +0700

    dart

--- 4_input.dart
+++ 4_input.dart
@@ -2,10 +2,10 @@ import 'dart:io';
 
 void main() {
   stdout.write("masukan password anda: ");
-  var input1 = stdin.readLineSync();
+  var input1 = stdin.readLineSync()!;
   while (true) {
     stdout.write("masukan lagi password anda: ");
-    var input2 = stdin.readLineSync();
+    var input2 = stdin.readLineSync()!;
     print(("======================="));
     if (input2 == input1) {
       print("-----------------------");
