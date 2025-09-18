1cbc4c14d402825ddcbd3da42664563b1342257f
ghatraxion <agh.arrafif@gmail.com>
2025-09-18 13:42:07 +0700

    dart

--- 4_input.dart
+++ 4_input.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 void main() {
   print("\n==== SELAMAT DATANG DI PROGRAM INPUT MILIK AGHA ====");
   stdout.write("Masukan namamu!: ");
-  var name_input = stdin.readLineSync()!;
+  String name_input = stdin.readLineSync()!;
   print("----------------------------------------------------");
   stdout.write("Masukan umurmu!: ");
   int age_input = int.parse(stdin.readLineSync()!);
