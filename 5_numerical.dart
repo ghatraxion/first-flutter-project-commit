497f386d8affcfb88da582be7729492088b431b2
ghatraxion <agh.arrafif@gmail.com>
2025-09-19 09:24:39 +0700

    dart

---
+++ 5_numerical.dart
@@ -0,0 +1,12 @@
+/**num = tipe data number bisa int atau double */
+void main() {
+  num a = 10; // integer
+  num b = 3.14; // double
+  num hex = 0xDEADBEEF; // hexadecimal integer
+
+  print("\n=== NUMERICAL ===");
+  print("variable num a = $a");
+  print("variable num b = $b");
+  print("variable num hex = $hex");
+  print("=== FINISHED! ===\n");
+}
---
+++ 6_string.dart
@@ -0,0 +1,12 @@
+/**didalam string ada single quote dan double quote 
+ * contoh singlr quote = 'hello world'
+ * contoh double quote = "hello world"
+*/
+void main() {
+  String single_quote = 'hello world';
+  String double_quote = "hello world";
+  print("\n=== STRING ===");
+  print("Contoh single quote = $single_quote");
+  print("Contoh double quote = $double_quote");
+  print("=== FINISHED! ===\n");
+}
