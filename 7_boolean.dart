37febca868e6d344cecdd93322ff2ee1aeaed9cc
ghatraxion <agh.arrafif@gmail.com>
2025-09-19 10:25:20 +0700

    dart

--- 7_boolean.dart
+++ 7_boolean.dart
@@ -1,7 +1,7 @@
 /**
  * simbol ! menandakan not atau bukan
  * 
- * !i love you = i don't love you
+ * !i love you != i don't love you
  * 
  * tipe data boolean hanya memiliki dua nilai yaitu:
  * true dan false
@@ -10,7 +10,8 @@
 bool is_true = true;
 bool is_false = false;
 void main(List<String> args) {
-  print("=== BOOLEAN ===");
-  print(is_true);
-  print(is_false);
+  print("\n=== BOOLEAN ===");
+  print("isi variable is_true: $is_true");
+  print("isi variable is_false: $is_false");
+  print("=== PROGRAM SELESAI ===\n");
 }
