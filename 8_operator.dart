541fdf594c84b7ed8e6285ab408170a4df2c5cc3
ghatraxion <agh.arrafif@gmail.com>
2025-09-22 09:13:43 +0700

    dart

--- 8_operator.dart
+++ 8_operator.dart
@@ -12,11 +12,15 @@ modulus = %
 void main(List<String> args) {
   int number_one = 10;
   int number_two = 20;
+  var a = 1;
+  a++; //Nilai a bertambah 1
+  var b = 1;
+  b--; //Nilai b berkurang 1
 
   print("\n===-OPERATOR-===");
   print("number_one = $number_one");
   print("number_two = $number_two");
-  print("===----HASIL-OPERATOR----===")
+  print("===----HASIL-OPERATOR----===");
   // === Hasil penjumlahan(+) ===
   print("$number_one + $number_two = ${number_one + number_two}");
   // === Hasil pengurangan(-) ===
@@ -27,7 +31,16 @@ void main(List<String> args) {
   print("$number_one / $number_two = ${number_one / number_two}");
   // === Hasil modulus(%) ===
   print("$number_one % $number_two = ${number_one % number_two}");
+  // === Contoh penggunaan increment(++) ===
+  print("=== INCREMENT & DECREMENT ===");
+  print("Nilai a = $a");
+  print("Nilai a++ = ${a++}");
+  print("Nilai b = $b");
+  print("Nilai b-- = ${b--}");
+  print("=== FINISHED! ===\n");
+
   print("=== OPERATOR SELESAI ===\n");
+
 }
 /**
 === INCREMENT & DECREMENT ===
