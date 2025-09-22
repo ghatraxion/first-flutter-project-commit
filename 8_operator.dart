1df03c349f437a20533fe9651e12cfe30e596e93
ghatraxion <agh.arrafif@gmail.com>
2025-09-22 08:50:45 +0700

    dart

--- 8_operator.dart
+++ 8_operator.dart
@@ -1,6 +1,25 @@
 import 'dart:math';
 
+/**
+=== OPERATOR ===
+Pengunaan operator =
+penjumlahan = + 
+pengurangan = -
+perkalian = *
+pembagian = /
+modulus = %
+ */
 void main(List<String> args) {
   int number_one = 10;
   int number_two = 20;
+
+  print("\n=== OPERATOR ===");
+  print("number_one = $number_one");
+  print("number_two = $number_two");
+  print("$number_one + $number_two = ${number_one + number_two}");
+  print("$number_one - $number_two = ${number_one - number_two}");
+  print("$number_one * $number_two = ${number_one * number_two}");
+  print("$number_one / $number_two = ${number_one / number_two}");
+  print("$number_one % $number_two = ${number_one % number_two}");
+  print("=== OPERATOR SELESAI ===\n");
 }
