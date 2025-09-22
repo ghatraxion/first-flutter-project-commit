2b56a3160aa43b212131e60c9849531fea70504c
ghatraxion <agh.arrafif@gmail.com>
2025-09-22 08:53:30 +0700

    dart

--- 8_operator.dart
+++ 8_operator.dart
@@ -13,9 +13,10 @@ void main(List<String> args) {
   int number_one = 10;
   int number_two = 20;
 
-  print("\n=== OPERATOR ===");
+  print("\n===-OPERATOR-===");
   print("number_one = $number_one");
   print("number_two = $number_two");
+  print("===----HASIL-OPERATOR----===")
   print("$number_one + $number_two = ${number_one + number_two}");
   print("$number_one - $number_two = ${number_one - number_two}");
   print("$number_one * $number_two = ${number_one * number_two}");
