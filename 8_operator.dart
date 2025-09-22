ced5edbdffdda7c3fe3dbd14ee868038e93f610a
ghatraxion <agh.arrafif@gmail.com>
2025-09-22 08:55:34 +0700

    dart

--- 8_operator.dart
+++ 8_operator.dart
@@ -17,10 +17,15 @@ void main(List<String> args) {
   print("number_one = $number_one");
   print("number_two = $number_two");
   print("===----HASIL-OPERATOR----===")
+  // === Hasil penjumlahan(+) ===
   print("$number_one + $number_two = ${number_one + number_two}");
+  // === Hasil pengurangan(-) ===
   print("$number_one - $number_two = ${number_one - number_two}");
+  // === Hasil perkalian(*) ===
   print("$number_one * $number_two = ${number_one * number_two}");
+  // === Hasil pembagian(/) ===
   print("$number_one / $number_two = ${number_one / number_two}");
+  // === Hasil modulus(%) ===
   print("$number_one % $number_two = ${number_one % number_two}");
   print("=== OPERATOR SELESAI ===\n");
 }
