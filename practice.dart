043211a9c5730ce8f76f056821b6875069c57e2a
ghatraxion <agh.arrafif@gmail.com>
2025-09-24 08:33:43 +0700

    dart

---
+++ practice.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main() {
+  while (true) {
+    stdout.write("\n=== PROGRAM SEGITIGA BINTANG ===\n");
+    stdout.write("\n== Pilih Opsi: \n");
+    stdout.write("1. Segitiga Bintang kecil ke besar\n");
+    stdout.write("2. Segitiga Bintang besar ke kecil\n");
+    stdout.write("3. Exit\n");
+    stdout.write("Masukan pilihanmu: ");
+    int opsi = int.parse(stdin.readLineSync()!);
+    if (opsi == 1) {
+      stdout.
+    }
+  }
+}
