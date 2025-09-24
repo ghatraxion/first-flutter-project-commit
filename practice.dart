df13b0e74261086cd174a9df6e14b09752f465c9
ghatraxion <agh.arrafif@gmail.com>
2025-09-24 08:40:50 +0700

    dart

--- practice.dart
+++ practice.dart
@@ -10,7 +10,30 @@ void main() {
     stdout.write("Masukan pilihanmu: ");
     int opsi = int.parse(stdin.readLineSync()!);
     if (opsi == 1) {
-      stdout.
+      stdout.write("=== SEGITIGA BINTANG KECIL KE BESAR ===\n");
+      stdout.write("Masukan jumlah baris: ");
+      int baris = int.parse(stdin.readLineSync()!);
+      for (int i = 1; i <= baris; i++) {
+        for (int j = 1; j <= i; j++) {
+          stdout.write("* ");
+        }
+        print("");
+      }
+    } else if (opsi == 2) {
+      stdout.write("=== SEGITIGA BINTANG BESAR KE KECIL ===\n");
+      stdout.write("Masukan jumlah baris: ");
+      int baris = int.parse(stdin.readLineSync()!);
+      for (int i = baris; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+          stdout.write("* ");
+        }
+        print("");
+      }
+    } else if (opsi == 3) {
+      print("=== PROGRAM SELESAI ===");
+      break;
+    } else {
+      print("=== OPSI TIDAK VALID ===\n");
     }
   }
 }
