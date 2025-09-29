9d4279aefb2a298adea0029c4badb76aa1b03493
ghatraxion <agh.arrafif@gmail.com>
2025-09-29 14:13:02 +0700

    dart

---
+++ fundamental_dart/17_while_loop.dart
@@ -0,0 +1,13 @@
+/**
+ * while digunakan untuk mejalankan kode blok
+ * selama kondisi bernilai true
+ * cocok digunakan untuk perulangan dengan jumlah yang tidak diketahui
+ */
+
+void main() {
+  int i = 10;
+  while (i >= 0) {
+    print("ini angka ke: $i");
+    i--;
+  }
+}
