d42fb6361831923d1fab2ca0f06c2e05cefd5063
ghatraxion <agh.arrafif@gmail.com>
2025-09-29 10:54:47 +0700

    dart

--- tugas ustad/segitga_tengah.dart
+++ tugas ustad/segitga_tengah.dart
@@ -1,15 +1,1 @@
-import 'dart:io';
 
-void main() {
-  stdout.write("Masukkan tinggi segitiga: ");
-  int n = int.parse(stdin.readLineSync()!);
-
-  for (int i = 1; i <= n; i++) {
-    // cetak spasi di kiri
-    String spasi = " " * (n - i);
-    // cetak bintang (ganjil: 1, 3, 5, ...)
-    String bintang = "*" * (2 * i - 1);
-
-    print(spasi + bintang);
-  }
-}
