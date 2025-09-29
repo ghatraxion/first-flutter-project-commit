6b17ed79d8478a673922aeefa6b7211666cf9380
ghatraxion <agh.arrafif@gmail.com>
2025-09-29 11:23:17 +0700

    dart

--- mini project/triangle_printer.dart
+++ mini project/triangle_printer.dart
@@ -10,7 +10,7 @@ void main() {
   stdout.write("Enter the option number: ");
   int option = int.parse(stdin.readLineSync()!);
   if (option == 1) {
-    stdout.write("\n=== BIG TO SMALL TRIANGLE\n");
+    stdout.write("\n=== SMALL TO BIG TRIANGLE\n");
     stdout.write("How many line?: ");
     int line = int.parse(stdin.readLineSync()!);
     for (int i = 1; i <= line; i++) {
@@ -20,7 +20,14 @@ void main() {
       print("");
     }
   } else if (option == 2) {
+    stdout.write("\n=== BIG TO SMALL TRIANGLE\n");
+    stdout.write("How many line?: ");
     int line = int.parse(stdin.readLineSync()!);
-    for (int i = )
+    for (int i = line; i >= 1; i--) {
+      for (int j = 1; j <= i; j++) {
+        stdout.write("* ");
+      }
+      print("");
+    }
   }
 }
