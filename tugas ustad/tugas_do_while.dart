571b62b7cd26ea854a33867c7e692e2b50469a12
ghatraxion <agh.arrafif@gmail.com>
2025-09-30 13:12:17 +0700

    dart

---
+++ tugas ustad/tugas_do_while.dart
@@ -0,0 +1,14 @@
+/**
+ * buatkan angka yang menampilkan kelipatan 7 dan jumlahkan
+ * total kelipatannya
+ * range = 7 - 100
+ */
+
+void main() {
+  int tujuh = 0;
+  int hasil = 0;
+  do {
+    print("angka ke: $tujuh");
+    tujuh += 7;
+  } while (tujuh <= 100);
+}
