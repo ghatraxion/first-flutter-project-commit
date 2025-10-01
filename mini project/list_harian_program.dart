4cac0d61cc228585c4e5e7631a99218a3442a995
ghatraxion <agh.arrafif@gmail.com>
2025-10-01 09:41:49 +0700

    dart

--- mini project/list_harian_program.dart
+++ mini project/list_harian_program.dart
@@ -6,5 +6,31 @@ void main() {
   while (true) {
     print("\n== LIST HARIAN ===");
     print("Pilih opsi:");
+    print("1. Masukan list baru");
+    print("2. lihat list");
+    print("3. Hapus list");
+    print("4. exit program");
+    int opsi = int.parse(stdin.readLineSync()!);
+    if (opsi == 1) {
+      print("\n== TULIS LIST BARU ==");
+      stdout.write("Masukan list: ");
+      var new_list = stdin.readLineSync()!;
+      list.add(new_list);
+      print("list berhasil Di tambah!");
+    } else if (opsi == 2) {
+      print("\n== LIST HARIAN ===");
+      if (list.isEmpty) {
+        print("belum ada list yang ditambahkan!");
+      } else {
+        for (var see in list.asMap().entries) {
+          print("${see.key + 1}. ${see.value}");
+        }
+      }
+    } else if (opsi == 3) {
+      print("== HAPUS LIST ==");
+      if (list.isEmpty) {
+        print("beluma")
+      }
+    }
   }
 }
