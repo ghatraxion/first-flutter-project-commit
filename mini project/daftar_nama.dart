90b402da686f38d4cfbad467955ccb182f3e832b
ghatraxion <agh.arrafif@gmail.com>
2025-10-01 09:11:32 +0700

    dart

--- mini project/daftar_nama.dart
+++ mini project/daftar_nama.dart
@@ -2,6 +2,14 @@ import "dart:io";
 
 void main() {
   while (true) {
-    print("")
+    print("\n=== PROGRAM TAMBAH NAMA ===\n");
+    print("pilih opsi: ");
+    print("1. Tambah nama");
+    print("2. Lihat nama");
+    print("3. Exit program");
+    int opsi = int.parse(stdin.readLineSync()!);
+    if (opsi == 1) {
+      print
+    }
   }
 }
