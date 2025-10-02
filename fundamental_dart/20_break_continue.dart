dcb0e33bdb9c7bc1d4b1e58255ff26c9e688ea21
ghatraxion <agh.arrafif@gmail.com>
2025-10-02 10:08:08 +0700

    dart

---
+++ fundamental_dart/20_break_continue.dart
@@ -0,0 +1,9 @@
+/**
+ * break = digunakan untuk menghentikan seluruh loop
+ * continue = melewati iterasi dan melanjutkan iterasi berikutnya
+ * 
+ * break digunakan untuk keluar dari loop lebih awal,  sedangkan
+ * continue digunakan untuk melewati 1 iterasi, lalu lanjut ke iterasi
+ * berikutnya
+ */
+
