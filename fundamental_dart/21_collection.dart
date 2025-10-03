c14402472ef3235b89e3b3acf062fce4ab3c5dcf
ghatraxion <agh.arrafif@gmail.com>
2025-10-03 13:33:06 +0700

    dart

--- fundamental_dart/21_collection.dart
+++ fundamental_dart/21_collection.dart
@@ -1,6 +1,11 @@
 /**
- * List => berurutan, dibaca dari index 0
- * Set => acak
+ * List => berurutan, dibaca dari index 0, bisa sama (duplicate)
+ * Set => acak, unique
  * Map => key value, dibaca dari key
+ * 
+ * collection adalah struktur data yang digunakan untuk
+ * menyimpan banyak nilai sekaligus
  */
-void main() {}
+void main() {
+  print("\n=== COLLECTIONS ===\n");
+}
