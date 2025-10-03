8fb41cf92c2b3ddd57d3856103d0f102c9d47deb
ghatraxion <agh.arrafif@gmail.com>
2025-10-03 14:20:29 +0700

    dart

---
+++ challenge ustad/list.dart
@@ -0,0 +1,14 @@
+/**
+ * 1. buatlah list yang berisi 5 nama dari a - e
+ * dan tampilkan data nama index ke 3
+ * tambahkan nama baru
+ * cek apakah nama eka ada dalam list?
+ */
+
+void main() {
+  print("=====LIST=====");
+  List<String> nama = ["agha", "bagas", "caca", "deki", "eko"];
+  print(nama);
+  print(nama[3]);
+  nama.add("agkos");
+}
