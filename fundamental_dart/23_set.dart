6739a138b2500ac6a5b01b90fa1d604ca4bb08a5
ghatraxion <agh.arrafif@gmail.com>
2025-10-06 11:14:16 +0700

    dart

--- fundamental_dart/23_set.dart
+++ fundamental_dart/23_set.dart
@@ -8,6 +8,10 @@ void main() {
   Set<num> number = {1, 2, 3, 4, 5, 6, 7, 8, 9};
   print(number);
   number.add(8); //.add untuk menambahkan 1 elemen / data
-  number.addAll({9, 10, 11, 12, 13, 14}); // .addAll digunakan untuk menambahkan multi elemen / multi data *data lebih dari 1
+  number.addAll(
+    {9, 10, 11, 12, 13, 14},
+  ); // .addAll digunakan untuk menambahkan multi elemen / multi data *data lebih dari 1
   print(number);
+  number.remove(7);
+  print(number)
 }
