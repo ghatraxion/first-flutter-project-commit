80c21d15c665bb1c2accfd1f08f2f93dd334d339
ghatraxion <agh.arrafif@gmail.com>
2025-10-06 13:11:32 +0700

    dart

---
+++ challenge ustad/set.dart
@@ -0,0 +1,35 @@
+/**
+ * buatkan 2 set yang menampung nama nama minimal 7 nama
+ * dari masing masing set
+ * 1. tampilkan unionnya
+ * 2. tampilkan intersectionnya
+ * 3. tentukan panjang elemen keduanya
+ * 4. tentulan panjang elemen dari union
+ * 5. tentukan panjang elemen dari intersection
+ */
+
+void main() {
+  print("\n=== SET LEARNING TASK ===\n");
+  Set<String> namaKelasA = {
+    "agha",
+    "rafif",
+    "al",
+    "fahri",
+    "hafid",
+    "cean",
+    "arkham",
+  };
+  Set<String> namaKelasB = {
+    "rafif",
+    "javas",
+    "cean",
+    "hafid",
+    "affan",
+    "zaidan",
+    "zaka",
+  };
+  var unionClass = namaKelasA.union(namaKelasB);
+  print(
+    "hasil union dari variable \n1. $namaKelasA\n2. $namaKelasB\nadalah: $unionClass\n",
+  );
+}
