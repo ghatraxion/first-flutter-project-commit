be69f6ba0665887f7e600c710579379423b8dda9
ghatraxion <agh.arrafif@gmail.com>
2025-10-06 11:11:56 +0700

    dart

--- fundamental_dart/23_set.dart
+++ fundamental_dart/23_set.dart
@@ -5,7 +5,7 @@
  */
 
 void main() {
-  Set<num> number = {1, 2, 3, 4, 5, 6};
+  Set<num> number = {1, 2, 3, 4, 5, 6, 7, 8, 9};
   print(number);
   number.add(8); //.add untuk menambahkan 1 elemen / data
   number.addAll({9, 10, 11, 12, 13, 14});
