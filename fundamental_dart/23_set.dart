588d367d9c6951190e434e5ac558a736d2a14de5
ghatraxion <agh.arrafif@gmail.com>
2025-10-06 11:08:50 +0700

    dart

--- fundamental_dart/23_set.dart
+++ fundamental_dart/23_set.dart
@@ -3,3 +3,8 @@
  * sehingga yang sama hanya dipanggil sekali
  * jadi jika datanya sama, maka hanya dipanggil sekali
  */
+
+void main() {
+  Set<num> number = {1, 2, 3, 4, 5, 6, 7, 8};
+  print(number);
+}
