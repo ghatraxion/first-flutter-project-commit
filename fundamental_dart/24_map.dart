22d7b77f94f45ced525c62094119596bc4117a74
ghatraxion <agh.arrafif@gmail.com>
2025-10-06 14:00:50 +0700

    dart

--- fundamental_dart/24_map.dart
+++ fundamental_dart/24_map.dart
@@ -2,3 +2,12 @@
  * map menyimpan data dalam bentuk key dan value
  * mengakses data menggunakan key
  */
+
+void main() {
+  Map<String, String> plat = {
+    'A' = 'banten',
+    'B' = 'jakarta',
+    'D' = 'bandung',
+    'E' = 'cirebon',
+  };
+}
