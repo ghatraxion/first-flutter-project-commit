f448980fcacfa4de2c3321e90cadd473db9e6be1
ghatraxion <agh.arrafif@gmail.com>
2025-10-06 14:15:08 +0700

    dart

--- fundamental_dart/24_map.dart
+++ fundamental_dart/24_map.dart
@@ -17,4 +17,24 @@ void main() {
 
   print(plat);
   print(plat['b']); // untuk memanggil value dari key dengan cara variable['key]
+
+  /**
+   * menambahkan data ke dalam map:
+   * 1
+   */
+
+  plat['L'] = 'surabaya';
+  print(plat);
+
+  /**
+   * menampilkan seluruh keys
+   * menggunakan variable keys
+   */
+  print(plat.keys);
+
+  /**
+   * menampikal seluruh value
+   * menggunakan variable value
+   */
+  print(plat.values);
 }
