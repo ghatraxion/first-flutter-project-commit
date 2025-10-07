5feb6aa96937f7dc1eba224c8f73c3e394edb6bd
ghatraxion <agh.arrafif@gmail.com>
2025-10-07 11:17:21 +0700

    dart

---
+++ fundamental_dart/25_oop.dart
@@ -0,0 +1,16 @@
+class human {
+  String skin = '';
+  String hair = '';
+  double weight = 0;
+
+  human(this.skin, this.hair, this.weight);
+}
+
+void makan() {}
+void tidur() {}
+void berjalan() {}
+void belajar() {}
+void main() {
+  var ireng = human("hitam", "black", 72.0);
+  print(ireng.hair);
+}
