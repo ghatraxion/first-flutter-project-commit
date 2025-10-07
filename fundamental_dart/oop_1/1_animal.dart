806588a14683a058f2fa81dd5b75bb7b7fc138fa
ghatraxion <agh.arrafif@gmail.com>
2025-10-07 13:37:51 +0700

    dart

--- fundamental_dart/oop_1/1_animal.dart
+++ fundamental_dart/oop_1/1_animal.dart
@@ -1,8 +1,11 @@
 class animal {
+  // attribute (properties)
   String name = 'kuceng';
   String color = 'black and white';
   double weigth = 3;
   int age = 14;
+
+  // method / function / behavior(perilaku)
   void eat() {}
   void poop() {}
   void sleep() {}
