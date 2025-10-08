6ec64b9cb485d3dedecfdf856e62ab50607f6ad5
ghatraxion <agh.arrafif@gmail.com>
2025-10-08 13:16:13 +0700

    dart

--- fundamental_dart/26_constructor.dart
+++ fundamental_dart/26_constructor.dart
@@ -17,12 +17,8 @@ class animal {
     print("$name sedang tidur");
   }
 
+  //
   animal(this.name, this.color, this.weight);
-  // animal(String name, String color, double weight) {
-  //   this.name;
-  //   this.color;
-  //   this.weight;
-  // }
 }
 
 void main() {
