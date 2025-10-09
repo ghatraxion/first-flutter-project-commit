ba509d40a6a22c45a03678810faa8d7ea1589d09
ghatraxion <agh.arrafif@gmail.com>
2025-10-09 13:15:54 +0700

    dart

--- fundamental_dart/oop_2/2_hewan.dart
+++ fundamental_dart/oop_2/2_hewan.dart
@@ -6,7 +6,15 @@ class hewan {
   // constructor
   hewan(this.name, this.weight, this.umur);
   // method
-  void eat() {}
-  void sleep() {}
-  void poop() {}
+  void eat() {
+    print("$name sedang makan");
+  }
+
+  void sleep() {
+    print("$name sedang tidur");
+  }
+
+  void poop() {
+    print("$name sedang poop");
+  }
 }
