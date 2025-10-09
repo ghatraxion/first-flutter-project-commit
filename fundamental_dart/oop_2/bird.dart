8e8f4840e0fad2afa9fc0d17e018783d34739ab2
ghatraxion <agh.arrafif@gmail.com>
2025-10-09 14:08:01 +0700

    dart

--- fundamental_dart/oop_2/bird.dart
+++ fundamental_dart/oop_2/bird.dart
@@ -5,6 +5,16 @@ class Bird extends hewan {
   Bird(this.feather) : super('', 0.0, 0);
 
   void fly() {
-    
+    print("bird is fly");
   }
+
+  void color_feather() {
+    print("bird with $feather");
+  }
+}
+
+void main() {
+  var burung = Bird("white");
+  burung.fly();
+  burung.color_feather();
 }
