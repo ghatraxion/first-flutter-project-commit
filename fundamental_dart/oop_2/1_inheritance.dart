a5705c2577a2e4cdfef9ce49cec18e54f02fe709
ghatraxion <agh.arrafif@gmail.com>
2025-10-09 13:10:51 +0700

    dart

---
+++ fundamental_dart/oop_2/1_inheritance.dart
@@ -0,0 +1,15 @@
+void main() {
+  /**
+   * inheritance adalah kemampuan suatu program
+   * untuk membuat kelas baru  dari 
+   * kelas yang ada
+   * 
+   * konsepnya itu seperti anak mewarisi sifat dari
+   * orang tuanya.
+   * 
+   * yang menurunkan sifat disebut 
+   * kelas induk / parent class / super class
+   * kelas yang mewarisi disebut sebagai
+   * kelas anak (child class / subclass)
+   */
+}
