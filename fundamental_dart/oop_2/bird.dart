4147bddaade7563e6bbd1755b1890622dacb4bae
ghatraxion <agh.arrafif@gmail.com>
2025-10-09 13:56:25 +0700

    dart

---
+++ fundamental_dart/oop_2/bird.dart
@@ -0,0 +1,10 @@
+import '2_hewan.dart';
+
+class Bird extends hewan {
+  String feather;
+  Bird(this.feather) : super('', 0.0, 0);
+
+  void fly() {
+    
+  }
+}
--- fundamental_dart/oop_2/fish.dart
+++ fundamental_dart/oop_2/fish.dart
@@ -2,18 +2,19 @@ import '2_hewan.dart';
 
 class fish extends hewan {
   String? skincolor;
+
   fish(this.skincolor) : super('', 0.0, 0);
 
   void swim() {
-    print("$name is swim");
+    print("fish is swiming");
   }
 
   void skin() {
-    print("fish with skin color  $skincolor");
+    print("$skincolor fish is swim1");
   }
 }
 
 void main() {
-  var ikan = fish("black");
+  var ikan = fish('black');
   ikan.skin();
 }
